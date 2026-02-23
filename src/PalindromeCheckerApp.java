2f7edd17234b7d65ef9135b410e7a7139dc95174
tnaybabua <rahultanaybabu@gmail.com>
2026-02-23 10:32:01 +0530

    Added UseCase2 implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,27 @@
+//
+//
+////@author Rahul Tanay
+////@version 2.0
+//
 
 
-//@author Rahul Tanay
-//@version 1.0
 
 public class PalindromeCheckerApp {
     public static void main(String args[]){
-        System.out.println("Welcome to Palindrome Checker Mangement System");
-        System.out.println("Version :  1");
-        System.out.println("System initalized successfully");
+       String name = "madam";
+
+       boolean ispalindrome = true;
+       int len = name.length() -1;
+       for(int i = 0 ; i<(len/2) ; i++){
+           if(name.charAt(i) != name.charAt(len-i)){
+               ispalindrome =  false;
+           }
+       }
+
+       System.out.println("Is it a Palindrome? : " + ispalindrome);
 
 
 
     }
 }
+
