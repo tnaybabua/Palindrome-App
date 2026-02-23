87b4f065376e4ced3b97d49a9d78b7c8f7160eb4
tnaybabua <rahultanaybabu@gmail.com>
2026-02-23 09:50:09 +0530

    welcome screen done! v1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+
+
+//@author Rahul
+//@version 1.0
+
+public class PalindromeCheckerApp {
+    public static void main(String args[]){
+        System.out.println("Welcome to Palindrome Checker Mangement System");
+        System.out.println("Version :  1");
+        System.out.println("System initalized successfully");
+
+
+
+    }
+}
